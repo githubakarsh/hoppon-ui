be8d83cb7d40980df891ae4c91b949eb934563e6
Akarsh Saraff <saraffakarsh@gmail.com>
2025-04-28 15:11:47 +0530

    commit
    commit

--- src/features/events/eventList/EventList.tsx
+++ src/features/events/eventList/EventList.tsx
@@ -1,12 +1,10 @@
 import { Buttoncomponent } from "../../../components/pageComponents/button/ButtonComponent";
 import { InputField } from "../../../components/pageComponents/inputField/InputField";
-import { HopponParagraph } from 'hoppon-react-lib';
 
 export const EventList = () => {
     return <section>
         <InputField label="Event name" height="35px" width="500px" placeholder="Enter the event name"/>
         <Buttoncomponent label="Create event"/>
 
-        <HopponParagraph />
     </section>
 };
